55cb839d2724147574c7ad19285da4c1602efb79
Pumpkingshead <leo.blomdahl@outlook.com>
2024-12-03 15:30:54 +0100

    createEmployee method-overloading

--- Main.java
+++ Main.java
@@ -3,11 +3,6 @@ public class Main {
 
     public static void main(String[] args) throws ValidationException {
         MyCompany company = new MyCompany();
-<<<<<<< Updated upstream
-        
-=======
->>>>>>> Stashed changes
-
 
         
     }
