b990ed28d8b45eeaec744c1a74ddac1ad055ef18
Giovana <gicunhads@gmail.com>
2024-12-03 15:25:45 +0100

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ public class Main {
 
     public static void main(String[] args) throws ValidationException {
         MyCompany company = new MyCompany();
-        Employee katherine = EmployeeFactory.createRegularEmployee("Katherine Johnson", "IDa", 50000.00);
-        company.addEmployee("IDa");
+        
 
 
     }
