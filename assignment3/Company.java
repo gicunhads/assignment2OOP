22b0f508c308065bc6873bc0aa37e7b80fbdff86
tyra_balg <128725591+Tyraba@users.noreply.github.com>
2024-12-03 17:31:12 +0100

    Update Company.java

--- assignment3/Company.java
+++ assignment3/Company.java
@@ -47,7 +47,7 @@ public class Company extends AbstractCompany {
 }
 
     @Override
-    public String employeeDegreeDetails() { 
+    public String mapPerDegree()() { 
         int bsc = 0;
         int msc = 0;
         int phd = 0;
