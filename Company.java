1188eeeda07f08f3bd99224739382bbc0b0e031b
Giovana <gicunhads@gmail.com>
2024-12-03 09:43:29 +0100

    modified:   Company.java

--- Company.java
+++ Company.java
@@ -86,6 +86,8 @@ public abstract class Company {
     HashMap<String, Employee> dictEmployees = new HashMap<>(); 
     EmployeeFactory employeeFactory = new EmployeeFactory(); // employee factory
     
+    public abstract void addEmployee(Employee employee);
+
     public abstract void removeEmployee(String id); 
 
     public abstract void retriveEmployee(String id);
@@ -100,7 +102,7 @@ public abstract class Company {
 
     public abstract void updateName(String id, String newName);
 
-    public abstract void addEmployee(Employee employee);
+    public abstract void addEmployee(String employee);
 
     public abstract String getEmployeesSorted();
 
