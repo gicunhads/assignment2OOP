6992d85ba9aee56f598c639729108470f4210aaf
tyra_balg <128725591+Tyraba@users.noreply.github.com>
2024-12-04 17:24:13 +0100

    Update DoubleFormat.java

--- assignment3/DoubleFormat.java
+++ assignment3/DoubleFormat.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public class DoubleFormat {
     
     public static double doubleFormater(double input){
-        double output = Double.parseDouble(String.format(Locale.US, "%.2f", input));
+        double output = (Math.floor(salary*100))/100;
     
         return output;
     }
