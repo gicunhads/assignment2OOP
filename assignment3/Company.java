4e04aae94ee29b3b8e3bca136965796e661e44a8
Giovana <gicunhads@gmail.com>
2024-12-05 11:11:45 +0100

    modified:   assignment3/Company.java

--- assignment3/Company.java
+++ assignment3/Company.java
@@ -73,7 +73,7 @@ public class Company {
         for (Employee employee : dictEmployees.values()){
             totalNetSalary += employee.getNetSalary();
         }
-        return DoubleFormat.truncate(totalNetSalary);
+        return DoubleFormat.round(totalNetSalary);
 
     }
 
