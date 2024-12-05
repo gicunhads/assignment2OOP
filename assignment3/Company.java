a3ad7b8b575103a3c5d6151afe283da1aa853c0d
Giovana <gicunhads@gmail.com>
2024-12-05 12:05:39 +0100

    modified:   assignment3/Company.java

--- assignment3/Company.java
+++ assignment3/Company.java
@@ -84,6 +84,8 @@ public class Company {
         int msc = 0;
         int phd = 0;
         Map<String, Integer> employeeDegreeDetails = new HashMap<>();
+        
+
         for (Employee employee : dictEmployees.values()) {
             if (employee instanceof Manager manager) {
                 if (manager.getDegree().equalsIgnoreCase("bsc")) {
@@ -101,6 +103,7 @@ public class Company {
                 }
             }
         }
+        
         if (bsc > 0) {
             employeeDegreeDetails.put("BSc", bsc);
         }
